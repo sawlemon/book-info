bdc0d50ff9dc13d55e53e5295ff2c1480bac3ce4
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-04-15 17:00:31 +0530

    image not yet displayed

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,12 @@ import './card.style.css';
 
 export const Card = props => (
     <div className='card-container'>
-        {/* <img alt="what" src={'https://robohash.org/${props.books.id}?set=set2'}  /> */}
-        <h1>{props.books.name}</h1>
+        <img 
+            alt="what" 
+            // src={'https://robohash.org/${props.books.id}?set=set2&size=120x120'}  />
+            srcSet = {"https://robohash.org/${props.books.id}?set=set4  "}
+            />
+        <h1>{ props.books.name }</h1>
+        <h1>{ props.books.email }</h1>
     </div>
 );
