0cc2089ba27055b13da239dde8ddc46e5a2e6ad3
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-04-15 12:15:41 +0530

    can't add image path

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.style.css';
 
 export const Card = props => (
     <div className='card-container'>
-        <img alt="what" src={'https://robohash.org/${props.books.id}?set=set2'}  />
+        {/* <img alt="what" src={'https://robohash.org/${props.books.id}?set=set2'}  /> */}
         <h1>{props.books.name}</h1>
     </div>
 );
