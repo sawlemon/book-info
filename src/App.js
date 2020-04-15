d0008826620b1fd4bdc33d9cef3a40d2d68c0f63
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-04-15 19:22:48 +0530

    github pages hosting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { CardList } from './components/card-list/card-list.component'
-
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 class App extends Component{
   constructor(){
     super();
 
     this.state = {
       books : [
-      ]
+      ],
+      searchField : '',
     };
   }
 
@@ -19,12 +20,23 @@ class App extends Component{
     .then(books => this.setState({books:books}))
 
   }
+
+  handleChange = (e) => {
+    this.setState({searchField : e.target.value})
+  };
+
   render(){
+    const {books,searchField } = this.state;
+    const filteredBooks = books.filter(book => book.name.toLowerCase().includes(searchField.toLowerCase()));
     return(
       <div className="App">
-        <CardList books={this.state.books}>
-  
-        </CardList>
+      <h1>Book-Info</h1>
+      <SearchBox
+        placeholder="bookname" 
+        handleChange={this.handleChange} 
+      />
+        
+      <CardList books={filteredBooks}></CardList>
        
     </div>
     );
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './card.style.css';
 
-export const Card = props => (
+export const Card = props => ( 
     <div className='card-container'>
         <img 
             alt="what" 
-            // src={'https://robohash.org/${props.books.id}?set=set2&size=120x120'}  />
-            srcSet = {"https://robohash.org/${props.books.id}?set=set4  "}
+            // use back-tick inside src{} it is not single quotes
+            src = {`https://robohash.org/${props.books.id}?set=set2&size=180x180`}
             />
-        <h1>{ props.books.name }</h1>
-        <h1>{ props.books.email }</h1>
+        <h2>{ props.books.name }</h2>
+        <h5>{ props.books.email }</h5>  
     </div>
 );
