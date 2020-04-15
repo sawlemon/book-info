b9c70a58768b05e2274e3a8df564b188c82ccea5
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-04-15 19:29:10 +0530

    Updates

---
+++ precache-manifest.30f33cfbbde7267b4a9d3f15d17395f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c485db2054abbad4e40eccc6a15c5fc",
+    "url": "/book-info/index.html"
+  },
+  {
+    "revision": "270e12f791e95d6ec116",
+    "url": "/book-info/static/css/main.b17612ed.chunk.css"
+  },
+  {
+    "revision": "0803e501a1eff5923540",
+    "url": "/book-info/static/js/2.6d963240.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-info/static/js/2.6d963240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "270e12f791e95d6ec116",
+    "url": "/book-info/static/js/main.b68cb3be.chunk.js"
+  },
+  {
+    "revision": "6f88e9b4f1b639ac21a1",
+    "url": "/book-info/static/js/runtime-main.d2a6f527.js"
+  }
+]);
