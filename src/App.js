3b33361dfbd68fd006cd4f71011084494d93973e
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-04-15 11:27:51 +0530

    card-list components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
 
 class App extends Component{
   constructor(){
@@ -15,14 +16,17 @@ class App extends Component{
   componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => this.setState({books:users}))
+    .then(books => this.setState({books:books}))
 
   }
   render(){
     return(
       <div className="App">
-        {this.state.books.map(book => (
-        <h1 key={book.id}> {book.name} </h1>)) }
+        <CardList>
+          {this.state.books.map(book => (
+          <h1 key={book.id}> {book.name} </h1>)) }
+        </CardList>
+       
     </div>
     );
   }
